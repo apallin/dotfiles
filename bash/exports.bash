d9d9f1a60669077a68ec555491e860cd3f614a2c
Adam Pallin <apallin@lyft.com>
2017-10-20 12:51:48 -0700

    Adding correct sublime path

--- bash/exports.bash
+++ bash/exports.bash
@@ -1,4 +1,4 @@
-export PATH="${PATH}:$HOME/bin:/usr/local/sbin:$HOME/.rvm/bin:$HOME/Applications/Sublime Text.app/Contents/SharedSupport/bin"
+export PATH="${PATH}:$HOME/bin:/usr/local/sbin:$HOME/.rvm/bin:/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
 export BLOCKSIZE=1k
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
