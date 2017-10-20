dee5eb58c92e82bf2609e30e085e94d0a1e69ef3
Adam Pallin <apallin@lyft.com>
2017-10-20 12:57:59 -0700

    Adding changes

--- bash/exports.bash
+++ bash/exports.bash
@@ -1,4 +1,4 @@
-export PATH="${PATH}:$HOME/bin:/usr/local/sbin:$HOME/.rvm/bin:/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+export PATH=${PATH}:$HOME/bin:/usr/local/sbin:$HOME/.rvm/bin
 export BLOCKSIZE=1k
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
